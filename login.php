6a29974ebdbb6db68e66fa68481900b2135bfa6b
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2022-08-29 20:35:52 -0300

    first comit

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+$login = $_POST['login'];
+$entrar = $_POST['login'];
+$senha = md5($_POST['senha']);
+$connect = mysqli_connect("localhost", "root", "", "login");
+
+    if (isset($entrar)) {
+
+        $verifica = mysqli_query($connect, "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'") or die ("erro ao selecionar");
+        if (mysqli_num_rows($verifica)<=0) {
+            echo"<script language= 'javascript' type='text/javascript'>
+            alert('Login e/ou senha incorretos');window.location
+            .href='login.html';</script>";
+            die();
+        }else{
+            setcookie("login",$login);
+            header("Location:index.php");
+        }
+    }
+?>
